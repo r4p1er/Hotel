2497dca96e487686b7e513211822d751d1e8ba87
Alexander Tsyganok <tsyganok2015@gmail.com>
2024-05-25 22:36:51 +0300

    Created Role, Right and Permission enums; created User entity

---
+++ Identity/Identity.Domain/Enums/Right.cs
@@ -0,0 +1,13 @@
+namespace Identity.Domain.Enums;
+
+public enum Right
+{
+    ReadUsers,
+    ReadUserById,
+    ReadSelf,
+    CreateUser,
+    CreateManager,
+    UpdateSelf,
+    UpdatePassword,
+    BlockUser
+}
