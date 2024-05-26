d805f2d11f0b56baf697c7c5e6ff543636f142c7
Alexander Tsyganok <tsyganok2015@gmail.com>
2024-05-26 18:47:03 +0300

    Logic of right to permission mapping is moved to RightPermissionMapper

--- Identity/Identity.Domain/Entities/User.cs
+++ Identity/Identity.Domain/Entities/User.cs
@@ -16,76 +16,9 @@ public class User
     
     public string PhoneNumber { get; set; }
     
-    public string PasswordHash { get; private set; }
+    public string PasswordHash { get; set; }
     
     public Role Role { get; set; }
-    
-    public bool IsBlocked { get; set; }
-    
-    private static readonly Dictionary<Role, Dictionary<Right, Permission>> RolePermissionMap =
-        new Dictionary<Role, Dictionary<Right, Permission>>()
-        {
-            {
-                Role.User, new Dictionary<Right, Permission>()
-                {
-                    { Right.ReadUsers, Permission.NoAccess },
-                    { Right.ReadUserById, Permission.NoAccess },
-                    { Right.ReadSelf, Permission.Access },
-                    { Right.CreateUser, Permission.NoAuthorize },
-                    { Right.CreateManager, Permission.NoAccess },
-                    { Right.UpdateSelf, Permission.Access },
-                    { Right.UpdatePassword, Permission.Access },
-                    { Right.BlockUser, Permission.NoAccess }
-                }
-            },
-            {
-                Role.Manager, new Dictionary<Right, Permission>()
-                {
-                    { Right.ReadUsers, Permission.NoAccess },
-                    { Right.ReadUserById, Permission.NoAccess },
-                    { Right.ReadSelf, Permission.Access },
-                    { Right.CreateUser, Permission.NoAuthorize },
-                    { Right.CreateManager, Permission.NoAccess },
-                    { Right.UpdateSelf, Permission.Access },
-                    { Right.UpdatePassword, Permission.Access },
-                    { Right.BlockUser, Permission.NoAccess }
-                }
-            },
-            {
-                Role.Admin, new Dictionary<Right, Permission>()
-                {
-                    { Right.ReadUsers, Permission.Access },
-                    { Right.ReadUserById, Permission.Access },
-                    { Right.ReadSelf, Permission.Access },
-                    { Right.CreateUser, Permission.NoAuthorize },
-                    { Right.CreateManager, Permission.Access },
-                    { Right.UpdateSelf, Permission.Access },
-                    { Right.UpdatePassword, Permission.Access },
-                    { Right.BlockUser, Permission.Access }
-                }
-            },
-            {
-                Role.Service, new Dictionary<Right, Permission>()
-                {
-                    { Right.ReadUsers, Permission.Access },
-                    { Right.ReadUserById, Permission.Access },
-                    { Right.ReadSelf, Permission.NoAccess },
-                    { Right.CreateUser, Permission.NoAuthorize },
-                    { Right.CreateManager, Permission.NoAccess },
-                    { Right.UpdateSelf, Permission.NoAccess },
-                    { Right.UpdatePassword, Permission.NoAccess },
-                    { Right.BlockUser, Permission.NoAccess }
-                }
-            }
-        };
 
-    public void ChangePassword(string password)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Permission HasRight(Right right)
-    {
-        return RolePermissionMap[Role][right];
-    }
+    public bool IsBlocked { get; set; } = false;
 }
