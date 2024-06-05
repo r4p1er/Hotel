13945970eb72d014af2cfe2c841c791b42eb607e
Alexander Tsyganok <tsyganok2015@gmail.com>
2024-06-05 14:05:15 +0300

    Created service collection extensions to add infrastrusture services

---
+++ Identity/Identity.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+using Identity.Application.Interfaces;
+using Identity.Infrastructure.Database;
+using Identity.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection collection)
+    {
+        collection.AddSingleton<IPasswordHasher, PasswordHasher>();
+        collection.AddScoped<ApplicationContext>();
+        collection.AddScoped<IUsersRepository, UsersRepository>();
+
+        return collection;
+    }
+}
