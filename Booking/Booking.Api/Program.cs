58b479244ac8b57878727752bd350ed776c2dd91
Alexander Tsyganok <tsyganok2015@gmail.com>
2024-06-23 18:03:36 +0300

    Added Booking.Api assembly; created as other api assemblies

---
+++ Booking/Booking.Api/Program.cs
@@ -0,0 +1,23 @@
+using Booking.Infrastructure;
+
+namespace Booking.Api;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        await host.MigrateDatabaseAsync();
+        
+        await host.RunAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
---
+++ Booking/Booking.Api/Startup.cs
@@ -0,0 +1,48 @@
+using Booking.Api.Middlewares;
+using Booking.Domain;
+using Booking.Infrastructure;
+
+namespace Booking.Api;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _environment;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+    {
+        _configuration = configuration;
+        _environment = environment;
+    }
+
+    public void ConfigureServices(IServiceCollection collection)
+    {
+        collection.AddControllers();
+        
+        collection.AddEndpointsApiExplorer();
+        collection.AddSwagger();
+
+        collection.AddAuth(_configuration["Auth:Key"]!);
+
+        collection.AddDomain();
+        collection.AddInfrastructure(_configuration["Connection:Default"]!);
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+        
+        if (_environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+        
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+}
