42cdfd071842249248b5b195aa3fa44e2a2e6804
Alexander Tsyganok <tsyganok2015@gmail.com>
2024-07-18 16:57:20 +0300

    Refactoring: update RabbitHostedService to use IServiceScopeFactory instead of IRoomService and inject service scope

--- Managing/Managing.Infrastructure/HostedServices/RabbitHostedService.cs
+++ Managing/Managing.Infrastructure/HostedServices/RabbitHostedService.cs
@@ -2,11 +2,12 @@ using System.Text.Json;
 using Hotel.Shared.Interfaces;
 using Hotel.Shared.Models;
 using Managing.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Managing.Infrastructure.HostedServices;
 
-public class RabbitHostedService(IRabbitService rabbit, IRoomService roomService) : BackgroundService
+public class RabbitHostedService(IRabbitService rabbit, IServiceScopeFactory scopeFactory) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -19,10 +20,15 @@ public class RabbitHostedService(IRabbitService rabbit, IRoomService roomService
                 var roomsId = JsonSerializer.Deserialize<List<Guid>>(message.Data, serializerOptions);
                 var result = new List<object>();
 
-                foreach (var id in roomsId!)
+                using (var scope = scopeFactory.CreateScope())
                 {
-                    var room = await roomService.GetById(id);
-                    result.Add(new { Id = id, Name = room.Summary });
+                    var roomService = scope.ServiceProvider.GetRequiredService<IRoomService>();
+                    
+                    foreach (var id in roomsId!)
+                    {
+                        var room = await roomService.GetById(id);
+                        result.Add(new { Id = id, Name = room.Summary });
+                    }
                 }
 
                 rabbit.SendMessage(new RabbitMessage
@@ -34,9 +40,10 @@ public class RabbitHostedService(IRabbitService rabbit, IRoomService roomService
                 });
             }
         });
-
+        
         while (!stoppingToken.IsCancellationRequested)
         {
+            await Task.Delay(1000, stoppingToken);
         }
         
         rabbit.Close();
